27b6b398ea409f9e2091770658752b7dade8cfe4
kevin <dng_lee@163.com>
2022-06-15 18:08:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const {
 
 const ViewPagerAndroid = require('react-native-pager-view');
 const TimerMixin = require('react-timer-mixin');
-const ViewPager = require('react-native-pager-view');
+import PagerView from 'react-native-pager-view';
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
 
 const AnimatedViewPagerAndroid = Platform.OS === 'android' ?
-  Animated.createAnimatedComponent(ViewPager) :
+  Animated.createAnimatedComponent(PagerView) :
   undefined;
 
 const ScrollableTabView = createReactClass({
